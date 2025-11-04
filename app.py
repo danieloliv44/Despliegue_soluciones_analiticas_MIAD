77b455afd71edb9a6724e6175544134fe5bd98d0
Daniel Amador <da.amador@uniandes.edu.co>
2025-11-03 19:14:16 -0500

    nueva función para cargar datos

--- app.py
+++ app.py
@@ -20,8 +20,14 @@ app.config.suppress_callback_exceptions = True
 
 
 # Load data from csv
+import pandas as pd
+
 def load_data():
-    # To do: Completar la función 
+    df = pd.read_csv("datos_energia.csv")
+    df["time"] = pd.to_datetime(df["time"])
+    df.set_index("time", inplace=True)
+    return df
+
     
 
 # Cargar datos
